b82c40c2211452f4f24eee29543772190013e426
Vincent Zheng <zhengye1@mail.utoronto.ca>
2015-12-27 00:02:01 -0500

    Temporary add the Employee in controller class, will finish the rest tmr

--- src/main/java/com/vincent/springmvc/controller/EmployeeController.java
+++ src/main/java/com/vincent/springmvc/controller/EmployeeController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vincent.springmvc.model.Employee;
 import com.vincent.springmvc.service.EmployeeService;
 
 @Controller
@@ -27,4 +30,8 @@ public class EmployeeController {
 		return "employee";
 	}
 	
+	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+	public ModelAndView addEmployee(ModelMap model){
+		return new ModelAndView("addEmployee", "command", new Employee());
+	}
 }
