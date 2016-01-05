e6d5b053ad6ab1a339e14afd83442d96e2e3a2c8
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-01-05 00:37:16 -0500

    Trying add security, will continue tmr

--- src/main/java/com/vincent/springmvc/controller/EmployeeController.java
+++ src/main/java/com/vincent/springmvc/controller/EmployeeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.vincent.springmvc.model.Employee;
 import com.vincent.springmvc.service.EmployeeService;
